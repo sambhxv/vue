fa589849f61264606dd1f054915c978f63c210ca
Sambhav Saxena <sambhavsaxena02@gmail.com>
2025-02-28 23:39:45 +0530

    added vue build setup
    Signed-off-by: Sambhav Saxena <sambhavsaxena02@gmail.com>

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hello!</h1>
+  <p>{{ data.count }}</p>
+  <button @click="increment">Increment</button>
+</template>
+
+<script setup> // composition API instead of options API, options work directly in the markup, requires build tools setup for transpiling
+  import {reactive} from "vue";
+
+  const data = reactive({
+    count: 0
+  })
+
+  const increment = () => {
+    data.count++
+  }
+</script>
