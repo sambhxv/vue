50b684d9d620d0559522b2be61f1bf7795451ee3
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2025-03-17 15:46:52 +0530

    added store app

---
+++ store/vite.config.js
@@ -0,0 +1,24 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import proxyOptions from './proxyOptions';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue()],
+	server: {
+		port: 8080,
+		host: '0.0.0.0',
+		proxy: proxyOptions
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src')
+		}
+	},
+	build: {
+		outDir: '../star_ecom/public/store',
+		emptyOutDir: true,
+		target: 'es2015',
+	},
+});
